f271433ade7385e69656eb31e3027210833771be
janageeth <69821658+janageeth@users.noreply.github.com>
2024-01-22 19:10:37 +1030

    making some changes alongside the code

--- dna-app.py
+++ dna-app.py
@@ -1,8 +1,34 @@
+# Importing all the libraries needed
 
-import yfinance as yf
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import altair as alt
+from PIL import Image
 
+##############################
+# Page Title
+##############################
+
+st.write("""
+# DNA Nucleotide Count Web App
+
+This app counts the nucleotide composition of query DNA!
+""")
+
+
+##############################
+# Input Text Box
+##############################
+
+#st.sidebar.header('Enter DNA sequence')
+st.header('Enter DNA sequence')
+sequence_input = ">DNA Query 2\nGAACAGTGGAGGCTAGGCAGTAGGGCTAGCTAAGCTAGCTAGCTAG"
+
+sequence = st.text_area("sequence_input", sequence_input, height=25)
+
+
+st.header('INPUT (DNA Query)')
+sequence
 
 
 
